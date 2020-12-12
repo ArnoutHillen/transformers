232765cc540f8b1ee5b96dab6a407507708691f3
Arnout Hillen <arnouthillen@gmail.com>
2020-12-12 17:16:17 +0100

    .

--- testing_arnout.py
+++ testing_arnout.py
@@ -1,11 +1,15 @@
-from typing import Tuple
-
-from transformers import BertTokenizer
 from transformers.models.bert.modeling_bert import BertModel
 from transformers.models.bert.tokenization_bert import BertTokenizer
+import torch
 
 if __name__ == "__main__":
     model = BertModel.from_pretrained("bert-base-cased", output_attentions=True, output_values=True)
     tokenizer = BertTokenizer.from_pretrained("bert-base-cased")
     inputs = tokenizer("Hello", return_tensors="pt")
     outputs = model(**inputs)
+    print(len(outputs["values"]))
+    print(outputs["values"][0].shape)
+    values = outputs["values"]
+    values = torch.stack(values).squeeze()
+    values = values.detach().numpy()
+    print(values.shape)
